6bfb64dd9c3132b3ca4cf21ec696c36b118f99e5
Max Matthews <max@mzmtech.com>
2021-12-30 17:16:13 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./ZaneTechLLC.png";
+import "./App.scss";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const Home = () => {
+	return (
+		<div>
+			<img src={logo} className={"logo"} alt={"Zane Tech LLC Mentoring"} />
+			<p>Custom software consulting, app development, and mentoring.</p>
+			<p>Currently in development: SlackTunes</p>
+		</div>
+	);
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Routes>
+					<Route path="/">
+						<Route index element={<Home />} />
+					</Route>
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
